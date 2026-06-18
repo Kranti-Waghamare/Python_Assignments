48a522d6704728e2a60dab05213ff5c07544ba9a
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-19 01:40:21 +0530

    program to display value, type and memory address of variable

---
+++ DataDemo.py
@@ -0,0 +1,21 @@
+#program to display value, type and memory address of variable
+
+no = 51
+
+print(no)
+print(type(no))
+print(id(no))
+
+
+A = "First code in python"
+
+print(A)
+print(type(A))
+print(id(A))
+
+
+value = 57.89
+
+print(value)
+print(type(value))
+print(id(value))
