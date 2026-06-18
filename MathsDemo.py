789fd03f6acf43e789b3392fc9dda6dbe1cc0341
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-19 01:52:46 +0530

    write a program that accepts two numbers from user and prints their addition, substraction, multiplication, division.

---
+++ MathsDemo.py
@@ -0,0 +1,12 @@
+#write a program that accepts two numbers from user and prints their addition, substraction, multiplication, division.
+
+print("Enter the first number : ")
+No1 = int(input())
+
+print("Enter the second number : ")
+No2 = int(input())
+
+print("Addition is : ", No1 + No2)
+print("Substraction is : ", No1 - No2)
+print("Multiplication is : ", No1 * No2)
+print("Division is : ", No1 / No2)
