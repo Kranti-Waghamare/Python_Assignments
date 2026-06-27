dfc02ebc6134ba35e0047c0bb67a8c42612beb4c
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-27 15:44:04 +0530

    prints square of the number

--- Assignment_9/program3.py
+++ Assignment_9/program3.py
@@ -1,15 +1,22 @@
 #write a program which accepts one number from user and prints square of that number.
 
 def SquareNumber(No):
-    print("square of that number is : ", No * No)
+    Ans = 0
+
+    Ans = No * No
+
+    return Ans
 
 def main():
     value = 0
+    Ret = 0
 
     print("Enter number :")
     Value = int(input())
 
-    SquareNumber(Value)
+    Ret = SquareNumber(Value)
+
+    print("Square of the number is : ", Ret)
 
 if __name__ == "__main__":
     main()
