7583cbd37d6710e72a89fd4b7101116000c43afb
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-02 18:38:11 +0530

    accepts the one number from and prints sum of digits.

---
+++ Assignment_11/program3.py
@@ -0,0 +1,27 @@
+#write a program which accepts the one number from and prints sum of digits.
+#Input : 123
+#Output : 6
+
+def SumDigits(No):
+    Digit = 0
+    Sum = 0
+
+    while(No > 0):
+        Digit = No % 10
+
+        Sum = Sum + Digit
+
+        No = No // 10
+
+    return Sum    
+
+def main():
+    print("Enter the number : ")
+    Value = int(input())
+
+    Ret = SumDigits(Value)
+
+    print(Ret)
+
+if __name__ == "__main__":
+    main()
