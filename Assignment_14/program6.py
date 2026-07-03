2b1351ae92379f67e7a7ff3d1302129d2e467bcd
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-03 13:41:21 +0530

    accepts one number and return true if number is odd otherwise false

---
+++ Assignment_14/program6.py
@@ -0,0 +1,14 @@
+#write a program which accepts one number and return true if number is odd otherwise false
+
+Odd = lambda No : (No % 2 != 0)
+
+def main():
+    print("Enter the number : ")
+    Value = int(input())
+
+    Ret = Odd(Value)
+
+    print(Ret)
+
+if __name__ == "__main__":
+    main()
