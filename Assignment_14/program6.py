6c7536e8442c2c7440e3a1e217684903487dc225
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-03 13:55:39 +0530

    lambda function which accepts one number and return true if number is odd otherwise false

--- Assignment_14/program6.py
+++ Assignment_14/program6.py
@@ -1,4 +1,4 @@
-#write a program which accepts one number and return true if number is odd otherwise false
+#write a lambda function which accepts one number and return true if number is odd otherwise false
 
 Odd = lambda No : (No % 2 != 0)
 
