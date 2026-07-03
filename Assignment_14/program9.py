352b60ec42e036f82f73fb1e251551492c1f1343
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-03 13:49:32 +0530

    accepts two number and returns Addition.

---
+++ Assignment_14/program9.py
@@ -0,0 +1,17 @@
+#write a program which accepts two number and returns Addition.
+
+Mult = lambda No1, No2: No1 + No2
+
+def main():
+    print("Enter the first number : ")
+    Value1 = int(input())
+
+    print("Enter the second number : ")
+    Value2 = int(input())
+
+    Ret = Mult(Value1, Value2)
+
+    print(Ret)
+
+if __name__ == "__main__":
+    main()
