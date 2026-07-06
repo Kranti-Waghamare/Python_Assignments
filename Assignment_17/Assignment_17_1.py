6099af21c4c99ac18bee25fc7a8c9af51a162892
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-06 17:24:36 +0530

    Module which contains arithmetic oprations

---
+++ Assignment_17/Assignment_17_1.py
@@ -0,0 +1,33 @@
+#create a module named as arithmetic which contains 4 functions as Add() for addition,Sub() for substraction,Mult() for multiplication, Div() for division. 
+#All function accepts two parameters as number and perform the operation. 
+#write on python program which call all the functions from arithmetic module by accepting the parameters from user.
+
+def Add(No1, No2):
+    Ans = 0
+
+    Ans = No1 + No2
+
+    return Ans 
+
+def Sub(No1, No2):
+    Ans = 0
+
+    Ans = No1 - No2
+
+    return Ans
+
+def Mult(No1, No2):
+    Ans = 0
+
+    Ans = No1 * No2
+
+    return Ans
+
+
+def Div(No1, No2):
+    Ans = 0
+
+    Ans = No1 / No2
+
+    return Ans 
+
