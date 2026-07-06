675d1ca268ca7340df961678f97bd56b925f30c7
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-06 17:03:33 +0530

    write a program which display 5 times marvellous on consle.

---
+++ Assignment_16/Assignment_16_4.py
@@ -0,0 +1,11 @@
+#write a program which display 5 times marvellous on consle.
+
+def main():
+    print("Marvellous")
+    print("Marvellous")
+    print("Marvellous")
+    print("Marvellous")
+    print("Marvellous")
+
+if __name__ == "__main__":
+    main()
