efaa241c737b005353db6f4bc69fe4d12a8f192d
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-06 18:05:26 +0530

    display pattern

--- Assignment_17/Assignment_17_2.py
+++ Assignment_17/Assignment_17_2.py
@@ -13,7 +13,7 @@ def DisplayPattern(Row, Col):
     for i in range(1, Row + 1):
         for j in range(1, Col + 1):
             print("*", end = "  ")
-        print("\n")
+        print()
 
 def main():
     print("Enter the number of Rows : ")
