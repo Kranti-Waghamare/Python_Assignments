ac51852ad327f8d62e9561aa551743f0ccac64de
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-06 17:07:28 +0530

    write a program which accept name from user and display length of its name.

---
+++ Assignment_16/Assignment_16_10.py
@@ -0,0 +1,15 @@
+#write a program which accept name from user and display length of its name.
+#Input : Marvellous
+#Output : 10
+
+def DisplayLen(name):
+    print(len(name))
+
+def main():
+    print("Enter the name : ")
+    Name = input()
+
+    DisplayLen(Name)
+
+if __name__ == "__main__":
+    main()
