7df6498ff9c9ad2089a4e68b4fcb99d22f826b28
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-06 20:04:26 +0530

    Display pattern

---
+++ Assignment_17/Assignment_17_8.py
@@ -0,0 +1,31 @@
+'''
+write a program which accept one number and display below pattern
+Input : 5
+Output :    1      
+            1   2   
+            1   2   3   
+            1   2   3   4    
+            1   2   3   4   5
+'''
+
+def DisplayPattern(Row, Col):
+
+    for i in range(1, Row + 1):
+        for j in range(1, Col + 1):
+            if(i >= j):
+                print(j, end = "  ")
+
+        print()
+
+
+def main():
+    print("Enter number of Rows : ")
+    Value1 = int(input())
+
+    print("Enter the number of Columns : ")
+    Value2 = int(input())
+
+    DisplayPattern(Value1, Value2)
+
+if __name__ == "__main__":
+    main()
